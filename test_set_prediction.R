7fa2e60a530f24248a1b03d4efce0b3be52fd5be
henfiber <henfiber@local>
2015-04-27 02:25:42 +0300

    also add test set prediction script and answers

---
+++ test_set_prediction.R
@@ -0,0 +1,13 @@
+# Algorithm prediction
+
+#answers = rep("A", 20)
+
+pml_write_files = function(x){
+     n = length(x)
+     for(i in 1:n){
+          filename = paste0("./Project_Prediction_Answers/problem_id_",i,".txt")
+          write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+     }
+}
+
+testPred = predict(modFit, testingRed2)
